77e2aa223f00d23591aa95e6fa73eb6782e328a5
Christian Vanderwall <christian@cvdub.net>
2025-03-22 20:40:32 -0700

    Fix type hint on Library abstract base class

--- src/djlib/clients/__init__.py
+++ src/djlib/clients/__init__.py
@@ -1,7 +1,9 @@
+from .abstract import Client
 from .rekordbox import RekordboxClient
 from .spotify import SpotifyClient
 
 __all__ = [
+    "Client",
     "RekordboxClient",
     "SpotifyClient",
 ]
--- src/djlib/libraries/abstract.py
+++ src/djlib/libraries/abstract.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 
+from ..clients import Client
+
 
 class Library(ABC):
     """Class for managing a music library."""
 
-    client_class: = None
+    client_class: type[Client] = None
 
     @abstractmethod
     async def refresh(self) -> None:
