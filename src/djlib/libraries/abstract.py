14f0aafe4595c4165429dab59cb31852997cf27c
Christian Vanderwall <christian@cvdub.net>
2025-03-22 20:53:56 -0700

    Open Client when starting Library class

--- src/djlib/libraries/abstract.py
+++ src/djlib/libraries/abstract.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from types import TracebackType
+from typing import Optional, Self, Type
 
 from ..clients import Client
 
@@ -7,6 +9,21 @@ class Library(ABC):
     """Class for managing a music library."""
 
     client_class: type[Client] = None
+
+    def __init__(self):
+        self._client = self.client_class()
+
+    async def __aenter__(self) -> Self:
+        await self._client.connect()
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[type[BaseException]],
+        exc_tb: Optional[TracebackType],
+    ):
+        await self._client.close()
 
     @abstractmethod
     async def refresh(self) -> None:
