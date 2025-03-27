3b109cfe4cc6d72f456197b1ee48a671a8615c30
Christian Vanderwall <christian@cvdub.net>
2025-03-27 17:22:48 -0400

    Switch from in memory to local database

--- src/djlib/config.py
+++ src/djlib/config.py
@@ -7,4 +7,4 @@ from platformdirs import user_cache_dir
 class Config:
     cache_directory: Path = user_cache_dir("djlib", ensure_exists=True)
     log_level: int = logging.INFO
-    database_file: str = ":memory:"
+    database_file: str = "db.sqlite3"
