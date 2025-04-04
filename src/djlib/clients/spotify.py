f5a96370e99c172f03325d5ec55977e71223d496
Christian Vanderwall <christian@cvdub.net>
2025-04-03 20:22:50 -0700

    Reduce concurrent spotify track downloads to 2

--- src/djlib/clients/spotify.py
+++ src/djlib/clients/spotify.py
@@ -32,7 +32,7 @@ from .abstract import Client, TrackExportError
 
 SPOTIFY_API_URL = "https://api.spotify.com/v1/"
 CONCURRENT_API_CALLS = 1
-CONCURRENT_DOWNLOADS = 6
+CONCURRENT_DOWNLOADS = 2
 DOWNLOAD_RETRIES = 10
 TRACK_STREAM_LOCK_DURATION = 10  # seconds
 
